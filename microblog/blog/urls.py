a3716bb68a187545b0d6e846faf35eae3815bb66
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2021-11-16 19:10:44 +0200

    Add basic views

---
+++ microblog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books', views.books_list, name='books-list'),
+    path('readers/', views.readers_list, name='readers-list'),
+    path('users/', views.users_list, name='users-list'),
+]
