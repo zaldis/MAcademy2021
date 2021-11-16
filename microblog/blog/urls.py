5f8bfe0a68e469fecb39cfb39bb0e0c0b996cad7
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2021-11-16 20:52:16 +0200

    Add bootstrap styles

--- microblog/blog/urls.py
+++ microblog/blog/urls.py
@@ -5,7 +5,7 @@ from blog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books', views.books_list, name='books-list'),
+    path('books/', views.books_list, name='books-list'),
     path('readers/', views.readers_list, name='readers-list'),
     path('users/', views.users_list, name='users-list'),
 ]
